415c8e594db9eadb7f4e4d3e91b802d80c4c5cb3
MyCloudStack <39018597+MyCloudStack@users.noreply.github.com>
2018-09-21 18:45:39 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+  <h1>Welcone to MyCloudStack</h1>
+  <img src='./image1.jpg' />
+</body>
+</html>
+?>
